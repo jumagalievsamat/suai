cebe2be7142b1248d549ae1ed03f5f6866a9cc15
jumag <poliprapilen@gmail.com>
2024-10-16 17:24:44 +0300

    test #2

--- lab_1_kuchin/conftest.py
+++ lab_1_kuchin/conftest.py
@@ -1,21 +1,50 @@
-import math
-count = 1
-each = ('test', '60', 15, '30', '1')
+tasks_list = [
+              ('tasks', 'time complited', 'time start', 'prio'),
+              ('test', '60', '15', '1'),
+              ('vito test', '150', '15', '1')
+             ]
+count = 'count'
 
-def output(each, count):
-    count = str(count)
-    print('_' * 65)
-    s_1 = '|' + count + ' ' * (abs(len(count) - 4)) + '|'
-    s_2 = each[0] + ' ' * abs(len(each[0]) - 30) + '|'
-    output_array = f'{s_1}{s_2}'
-    for i in range(1, len(each)):
-        value = str(each[i])
-        output_array += (value + ' ' * (abs(len(value) - 4)) + '|')
+def output_tasks(tasks):
+    global count
+    print('_' * 100)
+    for each in tasks:
+        space = len(each[0]) - 50
+        s_1 = '| ' + count + ' ' * (abs(len(count) - 6)) + '|'
+        if (len(each[0]) / 2) % 2 == 0: s_2 =  ' ' * abs(space//2 - 1) + each[0] + ' ' * abs(space//2 - 1) + '|'
+        else: s_2 = ' ' * abs(space//2) + each[0] + ' ' * abs(space//2 - 1)  + '|'
+        s_3 = each[1] + ' ' * (abs(len(each[1]) - 15)) + '|'
+        s_4 = each[2] + ' ' * (abs(len(each[2]) - 11)) + '|'
+        s_5 = each[3] + ' ' * (abs(len(each[3]) - 5)) + '|'
+        print(s_1, s_2, s_3, s_4, s_5)
+        if count == 'count':
+            count = '0'
+            print('_' * 100)
+        count = str(int(count) + 1)
 
-    for i in range(1, 10):
-        print(output_array)
-        if i == 1:
-            print('_' * 65)
-    print('_' * 65)
+    print('_' * 100)
 
-output(each, count)
+
+def input_tasks(tasks_list):
+    global count
+    if count == 'count':
+        count = '0'
+        count = str(int(count) + 1)
+        interval = input('введите интервал выполнения задачи: ')
+        cpu = input('Введите количество одновременно выполняемых задач: ')
+
+    print('Введите входные данные')
+    title_tasks = input('1 - Имя задачи: ')
+    time_complited = input('2 - время на выполнение этой задачи: ')
+    time_start = input('3 - время начала выполнения этой задачи: ')
+
+    prio = input('4 - Приоритет (1-5): ')
+    tasks_list.append((title_tasks, time_complited, time_start, prio))
+    action = input('Хотите еще ввести задачу?\n1 - ввести еще задачу\n0 - выйти в главное меню ')
+    if action == '1': input_tasks(tasks_list)
+    else: print(' ' * 100)
+
+
+input_tasks(tasks_list)
+print(tasks_list)
+output_tasks(tasks_list)
--- lab_1_kuchin/main.py
+++ lab_1_kuchin/main.py
@@ -1,32 +1,24 @@
-import time
 from conftest import *
 
-
-count = ''
 
 # output value
 cpu = ''
 done = ''
-tasks_list = [('test', '60', 15, '30', '1')]
+tasks_list = [('count', 'tasks', 'time complited', 'time start', 'interval', 'prio')]
 
 
 menu = True
 while menu:
-    action = input('1 - Запуск и ввод задачи\n2 - Показать очередь и выполненные задачи\nВведите значение: ')
+    action = input('1 - Запуск и ввод задачи\n2 - Показать очередь и выполненные задачи\n3 - autotest\n0 - выход\nВведите значение: ')
     match action:
         case '1':
-            # print('Введите входные данные')
-            # title_tasks = input('1 - Имя задачи: ')
-            time_complited = input('2 - время на выполнение этой задачи: ')
-            time_start = int(input('3 - время начала выполнения эт2ой задачи: '))
-            interval = input('4 - интервал выполнения задачи: ')
-            prio = input('5 - Приоритет (1-5): ')
-            tasks_list.append((time_complited, time_start, interval, prio))
+            input_tasks(tasks_list)
         case '2':
-            count = 1
             for each in tasks_list:
-                output(each, count)
+                output_tasks(each)
+        case '3':
+            print('еще не готово')
+        case '0':
+            break
 
-                # print(f'{count} Tasks title: \nTime_complited: {each[0]}\nTime_start: {each[1]}\nInterval: {each[2]}\nPrio: {each[3]}')
-                # print('-' * 50)
 
